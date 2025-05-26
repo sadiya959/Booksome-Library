4cc18296df98df217a4ab9369fde0f75b1b2a890
sadiya959 <sacdamamoud@gmail.com>
2025-05-26 22:02:44 +0300

    finished the dashboard of the library

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ let books = [
     isRead: true
   }
 ];
+
+document.querySelector('.ri-moon-line').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+})
+
+
+const hamburger = document.getElementById('hamburger');
+const sidebar = document.querySelector('.left-sidebar');
+const closeBtn = document.getElementById('close-sidebar');
+
+hamburger.addEventListener('click', () => {
+  sidebar.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+});
